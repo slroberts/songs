c9e7ab916cb123adf32291a44a82ad159746f163
Shomari Roberts <shomariroberts@gmail.com>
2021-06-11 15:14:28 -0400

    rendered song list to item to show in component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const SongList = () => {
-  return <div>SongList</div>;
+const SongList = ({ songs }) => {
+  const renderList = () => {
+    return songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  };
+
+  return <div className="ui divided list">{renderList()}</div>;
 };
 
-export default SongList;
+const mapStateToProps = (state) => {
+  return {
+    songs: state.songs,
+  };
+};
+
+export default connect(mapStateToProps)(SongList);
