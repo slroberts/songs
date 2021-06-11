1d7a65459a5e56f6152b7971ab5144533a388b5a
Shomari Roberts <shomariroberts@gmail.com>
2021-06-11 14:38:09 -0400

    combined reducers with combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,6 @@
-const songReducer = () => {
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
   return [
     { title: 'No Scrubs', duration: '4:05' },
     { title: 'Macarena', duration: '2:30' },
@@ -14,3 +16,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
